099adb59b690d5965300385f75d00bf221ddce02
iamsss <saurav9760@gmail.com>
2018-06-10 06:28:47 +0530

    Final Correction to glow led from net

--- main.cpp
+++ main.cpp
@@ -7,14 +7,13 @@
 const char ssid[] = "Connectify-me";
 const char password[] = "qwert12345";
 
-const int pin = 8;
  
 // WiFiClient object
 WiFiClient client;
  
 void setup() {
 
- pinMode(pin,OUTPUT);
+ pinMode(0,OUTPUT);
   // Initialize Serial
   Serial.begin(9600);
   delay(100);
@@ -48,10 +47,10 @@ void loop() {
       String payload = http.getString();   //Get the request response payload
       Serial.println(payload);
       if(payload == "ON"){                     //Print the response payload
-        digitalWrite(pin,HIGH);
+        digitalWrite(0,HIGH);
          Serial.println("Should Glow the Blub");
     }else{
-      digitalWrite(pin,LOW);
+      digitalWrite(0,LOW);
       
          Serial.println("Should not Glow the Blub");
     }
@@ -59,7 +58,68 @@ void loop() {
     http.end();   //Close connection
  
   }
+     //Send a request every 10 seconds
+  }
+}#include <ESP8266HTTPClient.h>
+
+#include <ESP8266WiFi.h>
+
+
+// WiFi config
+const char ssid[] = "Connectify-me";
+const char password[] = "qwert12345";
+
  
-  delay(5000);    //Send a request every 10 seconds
+// WiFiClient object
+WiFiClient client;
+ 
+void setup() {
+
+ pinMode(0,OUTPUT);
+  // Initialize Serial
+  Serial.begin(9600);
+  delay(100);
+ 
+  // Connect to WiFi
+  Serial.print("Connecting to ");
+  Serial.print(ssid);
+  WiFi.begin(ssid, password);
+  while ( WiFi.status() != WL_CONNECTED ) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+ 
+  // Show that we are connected
+  Serial.println("Connected!");
+}
+ 
+void loop() {
+ 
+  
+  if (WiFi.status() == WL_CONNECTED) { //Check WiFi connection status
+ 
+    HTTPClient http;  //Declare an object of class HTTPClient
+ 
+    http.begin("http://dry-brook-43259.herokuapp.com/bulbs/status");  //Specify request destination
+    int httpCode = http.GET();                                                                  //Send the request
+ 
+    if (httpCode > 0) { //Check the returning code
+ 
+      String payload = http.getString();   //Get the request response payload
+      Serial.println(payload);
+      if(payload == "ON"){                     //Print the response payload
+        digitalWrite(0,HIGH);
+         Serial.println("Should Glow the Blub");
+    }else{
+      digitalWrite(0,LOW);
+      
+         Serial.println("Should not Glow the Blub");
+    }
+ 
+    http.end();   //Close connection
+ 
+  }
+     //Send a request every 10 seconds
   }
 }
